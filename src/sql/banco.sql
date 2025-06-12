c56e2f94e93dc4d3613749fdec94685ab2c2792d
brunoserra99 <brunoserra_99@hotmail.com>
2025-06-12 02:23:32 -0300

    bd_usuario_unico

--- src/sql/banco.sql
+++ src/sql/banco.sql
@@ -37,21 +37,9 @@ CREATE TABLE corridas (
 
 -- Inserindo usuários fixos
 INSERT INTO passageiros (nome, email, senha) VALUES
-('Gabriel', 'gabriel@passageiro', '12345'),
-('Bruno', 'bruno@passageiro', '12345');
+('passageiro', 'passageiro@passageiro', '12345'),
+
 
 INSERT INTO motoristas (nome, cnh, email, senha) VALUES
-('Gabriel', '12345678900', 'gabriel@motorista', '12345'),
-('Bruno', '98765432100', 'bruno@motorista', '12345');
+('motorista', '12345678900', 'motorista@motorista', '12345'),
 
--- Passageiros adicionais
-INSERT INTO passageiros (nome, email, senha) VALUES
-('Ana Souza', 'ana@passageiro.com', '0987'),
-('Carlos Lima', 'carlos@passageiro.com', '0987'),
-('Fernanda Alves', 'fernanda@passageiro.com', '0987');
-
--- Motoristas adicionais
-INSERT INTO motoristas (nome, cnh, email, senha) VALUES
-('João Mendes', '32145678900', 'joao@motorista.com', '0987'),
-('Patrícia Rocha', '65478912300', 'patricia@motorista.com', '0987'),
-('Rafael Torres', '98732165400', 'rafael@motorista.com', '0987');
